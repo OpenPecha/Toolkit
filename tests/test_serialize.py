efec3466380ee530cf6a1b812fae932445cc6eec
tenzin <ten13zin@gmail.com>
2019-08-28 14:12:22 +0530

    add fixtures for tests

--- tests/test_serialize.py
+++ tests/test_serialize.py
@@ -1,10 +1,15 @@
-from openpoti.serializemd import SerializeMd
-
-op_path = 'usage/layer_output'
-test_op = 'W1OP000001'
-test_opf_path = f'{op_path}/{test_op}/{test_op}.opf'
+import pytest
+from openpoti.serialize import Serialize
 
 
-def test_readbaselayer():
-    smd = SerializeMd(test_opf_path)
-    assert isinstance(smd.baselayer, str)
+@pytest.fixture(scope='module')
+def serialize():
+    op_path = 'usage/layer_output'
+    test_op = 'W1OP000001'
+    test_opf_path = f'{op_path}/{test_op}/{test_op}.opf'
+    serialize = Serialize(test_opf_path)
+    return serialize
+
+
+def test_readbaselayer(serialize):
+    assert isinstance(serialize.baselayer, str)
