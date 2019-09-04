b319a840f129cca7e563d4853f620ba4f5bdb5f4
tenzin <ten13zin@gmail.com>
2019-09-04 15:43:04 +0530

    changed feeding test cases to parameterized fixture

--- tests/test_blupdate.py
+++ tests/test_blupdate.py
@@ -3,14 +3,15 @@ import pytest
 from openpoti.blupdate import Blupdate
 
 
+@pytest.fixture(params=[{'srcbl': 'abefghijkl',
+                         'dstbl': 'abcdefgkl',
+                         'expected_result': [(0,2,0), (2,5,2), (8,10,-1)]}])
+def test_cases(request):
+    return request.param
 
-@pytest.mark.parametrize('srcbl, dstbl, expected_result',
-                         [
-                             ('abefghijkl', 'abcdefgkl', [(0,2,0), (2,5,2), (8,10,-1)])
-                         ])
-def test_compute_cctv(srcbl, dstbl, expected_result):
-    updater = Blupdate(srcbl, dstbl)
+def test_compute_cctv(test_cases):
+    updater = Blupdate(test_cases['srcbl'], test_cases['dstbl'])
 
     result = updater.cctv
 
-    assert result == expected_result
+    assert result == test_cases['expected_result']
