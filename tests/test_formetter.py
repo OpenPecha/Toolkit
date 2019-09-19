11617de18a84e9e5e15cca218cc2c4794073a00e
tenzin <ten13zin@gmail.com>
2019-09-19 16:02:21 +0530

    add passing test for current fromatter

---
+++ tests/test_formetter.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from openpoti.formatter import OPFormatter
+
+m_text = Path('data/formatter/v2.txt').read_text()
+
+def test_build_layers():
+    formatter = OPFormatter()
+    
+    text = formatter.text_preprocess(m_text)
+    result = formatter.build_layers(text)
+
+    expected_result = {
+        'title': [0],
+        'yigchung': [193, 830],
+        'tsawa': [919, 1073],
+        'quotes': [1733, 1777],
+        'sapche': [1318, 1393]
+    }
+
+    print(result)
+    for layer, ann in expected_result.items():
+        if layer == 'title':
+            assert result[layer][0] == expected_result[layer][0]
+        else:
+            assert result[layer][0] == expected_result[layer]  
