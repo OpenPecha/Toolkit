0e90f829d39f09a5e255a6f6c81c2d2274ec3bab
tenzin <ten13zin@gmail.com>
2019-09-23 23:29:47 +0530

    add download command to download all the poti

---
+++ openpoti/cli.py
@@ -0,0 +1,78 @@
+'''
+TODO:
+1. Download catalog.csv
+2. Download all the OpenPotis
+3. poti layer <layer-name> -o <path to opf file>
+3. poti layers -o <path to opf file>
+'''
+import csv
+import requests
+import zipfile
+from io import BytesIO, StringIO
+from pathlib import Path
+
+import click
+from tqdm import tqdm
+
+from openpoti.serializemd import SerializeMd
+
+
+config = {
+    'CATALOG_URL': 'https://raw.githubusercontent.com/OpenPoti/openpoti-catalog/master/data/catalog.csv',
+    'OpenPotiRepo': 'https://github.com/OpenPoti/',
+    'CONFIG_PATH': '.openpoti/config/data_config'
+}
+
+
+@click.group()
+def cli():
+    pass
+
+
+def get_poti_list():
+    poti_list = []
+
+    # download the catalog
+    r = requests.get(config['CATALOG_URL'])
+    f = StringIO(r.content.decode('utf-8'))
+    
+    # parse the poti workid
+    reader = csv.reader(f, delimiter=',')
+    for row in reader:
+        if not row[0].startswith('W'): continue
+        poti_list.append(row[0])
+
+    return poti_list
+
+
+def download_poti(poti):
+    poti_url = f'https://github.com/OpenPoti/{poti}/archive/master.zip'
+
+    # download the poti
+    r = requests.get(poti_url)
+    z = zipfile.ZipFile(BytesIO(r.content))
+    z.extractall(config['data'])
+
+    # rename the poti
+    source = config['data']/f'{poti}-master'
+    target = config['data']/poti
+    source.replace(target)
+
+
+# Download openPoti
+@cli.command()
+@click.option('--save_dir', default='./openpoti')
+def download(save_dir):
+    # configure the data_path
+    config['data'] = Path(save_dir)
+
+    # download the repo
+    poti_list = get_poti_list()
+    for poti in poti_list[0:1]:
+        print(poti)
+        download_poti(poti)
+
+    # save data_path in data_config
+    config_path = Path(config['CONFIG_PATH'])
+    config_path.parent.mkdir(parents=True, exist_ok=True)
+    config_path.write_text(str(config['data'].resolve()))
--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
 from setuptools import setup, find_packages
 
-setup(name="openpoti", packages=find_packages())
+setup(
+    name="openpoti",
+    version="0.1.0",
+    author="Esukhia developers",
+    author_email="esukhiadev@gmail.com",
+    description="OpenPoti Toolkit allows state of the art for distributed standoff annotations on moving texts",
+    license="Apache2",
+    url="https://github.com/OpenPoti/openpoti-toolkit",
+    packages=find_packages(),
+
+    entry_points={
+        "console_scripts": ["openpoti=openpoti.cli:cli"] # command=package.module:function
+    },
+)
