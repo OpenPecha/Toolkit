e752a4cf20cec3d71e22fdb014d70436f183d55a
tenzin <ten13zin@gmail.com>
2019-11-15 14:31:26 +0530

    add test-case for kangyur parser

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -21,3 +21,24 @@ def test_build_layers():
 
     for layer, ann in expected_result.items():
         assert result[layer][0] == expected_result[layer]
+
+def test_kangyur_formatter():
+    m_test = Path('tests/data/formatter/kangyur_01.txt').read_text()
+    formatter = kangyurFormatter()
+    
+    text = formatter.text_preprocess(m_text)
+    result = formatter.build_layers(text)
+
+    expected_result = {
+        'pages': [(0, 24), (27, 676), (679, 2157)],
+        'lines': [
+            [(0, 24)],
+            [(27, 139),(141, 267), (269, 403), (405, 540), (542, 676)],
+            [(679, 870), (872, 1088), (1090, 1296), (1298, 1494), (1496, 1707), (1709, 1930), (1932, 2157)]
+        ],
+        'text_ids': [(27, 2157)],
+        'text_sub_ids': [[(27, 1351), (1352, 1494), (1495, 2157)]]
+    }
+
+    for layer in result:
+        assert result[layer] == expected_result[layer]
