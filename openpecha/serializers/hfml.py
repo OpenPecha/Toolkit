124b7f249ed576ef4807f62c74d54e927dfa9070
tenzin <ten13zin@gmail.com>
2020-01-23 01:27:23 +0530

    remove repeating code

--- openpecha/serializers/hfml.py
+++ openpecha/serializers/hfml.py
@@ -38,55 +38,43 @@ class SerializeHFML(Serialize):
 
         """
         only_start_ann = False
-        start_cc = 0
-        end_cc = 0
-        start_payload = ''
-        end_payload = ''
+        start_payload = '('
+        end_payload = ')'
         if ann['type'] == 'pagination':
-            start_cc, end_cc = self.__get_adapted_span(ann['span'], vol_id)
             start_payload = f'[{ann["page_index"]}] {ann["page_info"]}\n'
             only_start_ann = True
         elif ann['type'] == 'correction':
-            start_cc, end_cc = self.__get_adapted_span(ann['span'], vol_id)
             start_payload = '('
             end_payload = f',{ann["correction"]})'
         elif ann['type'] == 'peydurma':
-            start_cc, end_cc = self.__get_adapted_span(ann['span'], vol_id)
             start_payload = '#'
             only_start_ann = True
         elif ann['type'] == 'error_candidate':
-            start_cc, end_cc = self.__get_adapted_span(ann['span'], vol_id)
             start_payload = '['
             end_payload = ']'
         elif ann['type'] == 'book_title':
-            start_cc, end_cc = self.__get_adapted_span(ann['span'], vol_id)
             start_payload = '(k1'
             end_payload = ')'
         elif ann['type'] == 'author':
-            start_cc, end_cc = self.__get_adapted_span(ann['span'], vol_id)
             start_payload = '(au'
             end_payload = ')'
         elif ann['type'] == 'chapter_title':
-            start_cc, end_cc = self.__get_adapted_span(ann['span'], vol_id)
             start_payload = '(k3'
             end_payload = ')'
         elif ann['type'] == 'tsawa':
-            start_cc, end_cc = self.__get_adapted_span(ann['span'], vol_id)
             start_payload = '(m'
             end_payload = 'm)'
         elif ann['type'] == 'citation':
-            start_cc, end_cc = self.__get_adapted_span(ann['span'], vol_id)
             start_payload = '(g'
             end_payload = 'g)'
         elif ann['type'] == 'sabche':
-            start_cc, end_cc = self.__get_adapted_span(ann['span'], vol_id)
             start_payload = '(q'
             end_payload = 'q)'
         elif ann['type'] == 'yigchung':
-            start_cc, end_cc = self.__get_adapted_span(ann['span'], vol_id)
             start_payload = '(y'
             end_payload = 'y)'
         
+        start_cc, end_cc = self.__get_adapted_span(ann['span'], vol_id)
         self.add_chars(vol_id, start_cc, True, start_payload)
         if not only_start_ann:
             self.add_chars(vol_id, end_cc, False, end_payload)
