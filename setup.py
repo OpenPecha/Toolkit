c301bf5bf6d6a09a8113fe11848ea63625701578
tenzin <ten13zin@gmail.com>
2020-02-19 23:18:54 +0530

    bug fixe version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="openpecha",
-    version="0.4.4",
+    version="0.4.5",
     author="Esukhia developers",
     author_email="esukhiadev@gmail.com",
     description="OpenPecha Toolkit allows state of the art for distributed standoff annotations on moving texts",
