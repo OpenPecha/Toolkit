faa492865987bd48f3de3095e819bc3e1bf2ac25
tenzin <ten13zin@gmail.com>
2020-03-13 17:12:27 +0530

    fix: ocr-formatter first page index + version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="openpecha",
-    version="0.4.9",
+    version="0.4.10",
     author="Esukhia developers",
     author_email="esukhiadev@gmail.com",
     description="OpenPecha Toolkit allows state of the art for distributed standoff annotations on moving texts",
