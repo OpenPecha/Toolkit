b76cb3cd54a9918c11ac831dc27c83c03c61080e
Lou Guenier <louguenier@gmail.com>
2020-04-28 15:36:22 +0800

    Tibetan chunker

---
+++ openpecha/buda/tibetan_easy_chunker.py
@@ -0,0 +1,6 @@
+from openpecha.buda.chunker import StringChunker
+
+
+class TibetanEasyChunker(StringChunker):
+    def __init__(self, full_string, n):
+        StringChunker.__init__(self, full_string, "([སའངགདནབམརལཏ]\u0f7c།[^ཀ-ཬ]*|།།[^ཀ-ཬ༠-༩]*།[^ཀ-ཬ༠-༩]*)", n)
