5446e9dad5395168b32f93647e701090ef99b546
Lou Guenier <louguenier@gmail.com>
2020-05-01 15:14:48 +0800

    finalizing rdf

--- openpecha/serializers/rdf.py
+++ openpecha/serializers/rdf.py
@@ -1,12 +1,13 @@
 import re
-from openpecha.buda.rdf_setup import *
+from pathlib import Path
+from openpecha.serializers.rdf_setup import *
 from openpecha.buda.op_fs import OpenpechaFS
 from openpecha.buda.op_bare import OpenpechaBare
 from openpecha.buda.tibetan_easy_chunker import TibetanEasyChunker
 
 
 class Rdf:
-    def __init__(self, lname, path, from_git=True):
+    def __init__(self, lname, path=str(Path.home()/'.openpecha/data'), from_git=True):
         self.graphname = lname
         self.lod_ds = rdflib.Dataset()
         self.lod_g = self.lod_ds.graph(bdg[self.graphname])
@@ -49,9 +50,11 @@ class Rdf:
     """
     def set_instance(self):
         self.add_triplet(bdr[f'IE0{self.graphname}'], rdf.type, bdo["EtextInstance"])
-        self.get_base_volumes()
         if self.openpecha.meta.get('source_metadata'):
             self.parse_meta()
+        self.get_base_volumes()
+        self.set_adm()
+
 
     def parse_meta(self):
         sour = self.openpecha.meta['source_metadata']['id'].split(":")
@@ -59,9 +62,9 @@ class Rdf:
 
     def get_base_volumes(self):
         for volume in self.openpecha.base_layer.items():
+            self.set_etext_asset(volume)
             self.add_triplet(bdr[f'IE0{self.graphname}'], bdo['instanceHasVolume'],
                              bdr[f'VLIE0{self.graphname}_{volume[0].replace(".txt", "")}'])
-            self.set_etext_asset(volume)
             self.set_etext_ref(volume)
             self.set_etext(volume)
 
@@ -92,11 +95,34 @@ class Rdf:
         self.add_triplet(subject, bdo['eTextInInstance'], bdr[volume_name])
         self.add_triplet(subject, bdo['eTextIsVolume'], Literal(volume_number, datatype=XSD.integer))
         self.add_triplet(subject, rdfs.seeAlso, Literal(f'https://github.com/OpenPecha/{self.graphname}/', datatype=XSD.anyURI))
-        self.set_etext_page(volume)
+        self.set_etext_pages(volume)
         self.set_etext_chunks(volume)
 
-    def set_etext_page(self, volume):
-        pass
+    def set_etext_pages(self, volume):
+        volume_number = volume[0].replace(".txt", "")
+        annotations = self.openpecha.layers[volume_number]['pagination.yml'].annotations
+
+        for annotation in annotations:
+            self.set_etext_page(annotation, volume)
+
+    def set_etext_page(self, annotation, volume):
+        volume_name = f'IE0{self.graphname}_{volume[0].replace(".txt", "")}'
+        subject = bdr[f'EP{annotation["id"]}']
+        sequence = self.get_sequence(annotation['page_index'])
+        start = annotation['span']['start']
+        end = annotation['span']['end']
+
+        self.add_triplet(subject, rdf.type, bdo['EtextPage'])
+        self.add_triplet(subject, bdo['seqNum'], Literal(sequence, datatype=XSD.integer))
+        self.add_triplet(subject, bdo['sliceEndChar'], Literal(end, datatype=XSD.integer))
+        self.add_triplet(subject, bdo['sliceStartChar'], Literal(start, datatype=XSD.integer))
+
+        self.add_triplet(bdr[f'UT{volume_name}'], bdo['eTextHasPage'], subject)
+
+    @staticmethod
+    def get_sequence(page_index):
+        number = int(re.search(r'\d+', page_index).group())
+        return number * 2 if page_index[-1] == 'b' else (number * 2) - 1
 
     def set_etext_chunks(self, volume):
         volume_string = self.openpecha.base_layer[volume[0]]
@@ -117,6 +143,16 @@ class Rdf:
         self.add_triplet(subject, bdo['sliceStartChar'], Literal(start_char, datatype=XSD.integer))
 
         self.add_triplet(bdr[etext], bdo['eTextHasChunk'], subject)
+
+    def set_adm(self):
+        subject = bda[f'IE0{self.graphname}']
+        commit = self.openpecha.get_last_commit()
+
+        self.add_triplet(subject, rdf.type, adm['AdminData'])
+        self.add_triplet(subject, adm['adminAbout'], subject)
+        self.add_triplet(subject, adm['metadataLegal'], bda['LD_BDRC_CC0'])
+        self.add_triplet(subject, adm['openPechaCommit'], Literal(commit))
+        self.add_triplet(subject, adm['status'], bda['StatusReleased'])
 
     @staticmethod
     def get_chunk_index(string):
