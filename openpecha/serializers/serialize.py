c6ddc1ef754a29edc586ab00c475a0bdb63bdcaa
kaldan <kaldantenzin@gmail.com>
2020-05-22 15:51:44 +0530

    serialize epub done

--- openpecha/serializers/serialize.py
+++ openpecha/serializers/serialize.py
@@ -2,12 +2,11 @@ import os
 import yaml
 import requests
 
-import click
-
 from collections import namedtuple, defaultdict
 from pathlib import Path
 
-INFO = '[INFO] {}'
+INFO = "[INFO] {}"
+
 
 class Serialize(object):
     """
@@ -20,6 +19,7 @@ class Serialize(object):
 
     To use it, instantiate a concrete class with the path of the opf file, and call apply_layers() then get_result()
     """
+
     def __init__(self, opfpath, text_id=None, layers=None):
         self.opfpath = Path(opfpath)
         self.meta = self.get_meta_data()
@@ -28,8 +28,8 @@ class Serialize(object):
             self.text_spans = self.get_text_spans(text_id)
             self.base_layers = self.get_text_base_layer()
         else:
-            self.text_spans = {'v001': {'start': 0}} 
-            self.base_layers = {'v001': self.get_base_layer('v001')}
+            self.text_spans = {"v001": {"start": 0}}
+            self.base_layers = {"v001": self.get_base_layer("v001")}
         """
         The chars_toapply is an important piece of the puzzle here. Basically applying the changes to the string directly is a
         bad idea for several reasons:
@@ -61,43 +61,38 @@ class Serialize(object):
         # by convention, when it is None, all layers are applied in alphabetical order (?)
         self.layers = layers
 
-    
     def get_meta_data(self):
         opf_path = self.opfpath
-        meta = yaml.safe_load((opf_path/'meta.yml').open())
+        meta = yaml.safe_load((opf_path / "meta.yml").open())
         return meta
-
 
     def load_layer(self, fn):
         return yaml.safe_load(fn.open())
-
 
     def get_text_spans(self, text_id):
         """
         get spans of text
         """
-        index_layer = self.load_layer(self.opfpath/'index.yml')
-        for anno in index_layer['annotations']:
-            if anno['work'] == text_id:
+        index_layer = self.load_layer(self.opfpath / "index.yml")
+        for anno in index_layer["annotations"]:
+            if anno["work"] == text_id:
                 text_span = {}
-                for span in anno['span']:
-                    text_span[span['vol'].split('/')[-1]] = span['span']
+                for span in anno["span"]:
+                    text_span[span["vol"].split("/")[-1]] = span["span"]
                 return text_span
-
 
     def get_base_layer(self, vol_id):
         """
         return text for given span
         """
         if self.text_id:
-            vol_base = (self.opfpath/f"base/{vol_id}.txt").read_text()
-            start = self.text_spans[vol_id]['start']
-            end = self.text_spans[vol_id]['end']
-            return vol_base[start: end]
+            vol_base = (self.opfpath / f"base/{vol_id}.txt").read_text()
+            start = self.text_spans[vol_id]["start"]
+            end = self.text_spans[vol_id]["end"]
+            return vol_base[start:end]
         else:
-           vol_base = (self.opfpath/f"base/{vol_id}.txt").read_text()
-           return vol_base
-
+            vol_base = (self.opfpath / f"base/{vol_id}.txt").read_text()
+            return vol_base
 
     def get_text_base_layer(self):
         """
@@ -114,24 +109,26 @@ class Serialize(object):
             base_layers[vol_id] = self.get_base_layer(vol_id)
         return base_layers
 
-
     def apply_layer(self, vol_id, layer_id):
         """
         This reads the file opfpath/layers/layer_id.yml and applies all the annotations it contains, in the order in which they appear.
         I think it can be implemented in this class by just calling self.apply_annotation on each annotation of the file.
         """
-        layer = yaml.safe_load((self.opfpath/'layers'/vol_id/f'{layer_id}.yml').open())
-        for a in layer['annotations']:
-            if a['span']['end'] >= self.text_spans[vol_id]['start']:
-                a['type'] = layer_id
+        layer = yaml.safe_load((self.opfpath / "layers" / vol_id / f"{layer_id}.yml").open())
+        for a in layer["annotations"]:
+            if a["span"]["end"] >= self.text_spans[vol_id]["start"]:
+                a["type"] = layer_id
                 self.apply_annotation(vol_id, a)
-
 
     def get_all_layer(self, vol_id):
         """
         Returns all the layerid of layer from the layer directory
         """
-        return [layer.stem for layer in (self.opfpath/'layers'/vol_id).iterdir() if layer.suffix == '.yml']
+        return [
+            layer.stem
+            for layer in (self.opfpath / "layers" / vol_id).iterdir()
+            if layer.suffix == ".yml"
+        ]
 
     def apply_layers(self):
         """
@@ -144,50 +141,48 @@ class Serialize(object):
             for layer_id in self.layers:
                 self.apply_layer(vol_id, layer_id)
 
-
     def add_chars(self, vol_id, cc, frombefore, charstoadd):
         """
         This records some characters to add at a character coordinate (cc), either frombefore (from the left) or after. before is a boolean.
         """
         if cc not in self.chars_toapply[vol_id]:
-            self.chars_toapply[vol_id][cc] = ([],[])
-        if frombefore: # if from the left, layers should be applied in reverse order
+            self.chars_toapply[vol_id][cc] = ([], [])
+        if frombefore:  # if from the left, layers should be applied in reverse order
             self.chars_toapply[vol_id][cc][0].insert(0, charstoadd)
         else:
             self.chars_toapply[vol_id][cc][1].append(charstoadd)
-
 
     def apply_annotation(self, vol_id, annotation):
         """
         This applies the annotation given as argument. The annotation must contain at least a type
         """
-        raise NotImplementedError("The Serialize class doesn't provide any serialization, please use a subclass such ass SerializeMd")
-
+        raise NotImplementedError(
+            "The Serialize class doesn't provide any serialization, please use a subclass such ass SerializeMd"
+        )
 
     def __assign_line_layer(self, result, vol_id):
-
         def _get_page_index(line):
-            page_index = ''
+            page_index = ""
             i = 1
-            while line[i] != ']':
+            while line[i] != "]":
                 page_index += line[i]
                 i += 1
             return page_index
 
-        result_with_line = ''
-        page_index = ''
+        result_with_line = ""
+        page_index = ""
         n_line = 1
-        for line in result.split('\n'):
-            if not line: continue
-            if line[0] == '[' and line[1] != vol_id[0]:
+        for line in result.split("\n"):
+            if not line:
+                continue
+            if line[0] == "[" and line[1] != vol_id[0]:
                 page_index = _get_page_index(line)
                 n_line = 1
-            elif line[0] != '[':
-                line = f'[{page_index}.{n_line}]' + line
+            elif line[0] != "[":
+                line = f"[{page_index}.{n_line}]" + line
                 n_line += 1
-            result_with_line += line + '\n'
+            result_with_line += line + "\n"
         return result_with_line
-
 
     def get_result(self):
         """
@@ -200,11 +195,12 @@ class Serialize(object):
         # see https://waymoot.org/home/python_string/ where method 5 is good
         for vol_id, base_layer in self.base_layers.items():
             if self.text_id:
-                result += f'\n[{vol_id}]\n'
+                result += f"\n[{vol_id}]\n"
             i = 0
             for c in base_layer:
                 # UTF bom \ufeff takes the 0th index
-                if c == '\ufeff': continue
+                if c == "\ufeff":
+                    continue
                 if i in self.chars_toapply[vol_id]:
                     apply = self.chars_toapply[vol_id][i]
                     for s in apply[0]:
@@ -216,46 +212,9 @@ class Serialize(object):
                     result += c
                 i += 1
 
-        if 'pagination' in self.layers:
+        if "pagination" in self.layers:
             return self.__assign_line_layer(result, vol_id)
         else:
             return result
-        #return result
+        # return result
 
-    def serilize(self, pecha_id):
-        """ This module serialize .opf file to other format such as .epub etc. In case of epub,
-        we are using calibre ebook-convert command to do the conversion by passing our custom css template
-        and embedding our custom font. The converted output will be then saved in current directory
-        as {pecha_id}.epub.
-
-        Args:
-        pecha_id (string): Pecha id that needs to be exported in other format
-
-        """
-        out_fn = f'{pecha_id}.html'
-        # if self.meta['source_metadata']:
-        #     pecha_title = self.meta['source_metadata']['title']
-        # else:
-        pecha_title = self.meta['ebook_metadata']['title']
-        result = self.get_result()
-        result_lines = result.splitlines() # Result is split where there is newline as we are going to consider newline as one para tag
-        results = f'<html>\n<head>\n\t<title>{pecha_title}</title>\n</head>\n<body>\n'
-        for result_line in result_lines:
-            results += f'<p class="tibetan-regular-indented">{result_line}</p>\n'
-        results += '</body>\n</html>'
-        click.echo(results, file=open(out_fn, 'w'))
-        # logging
-        msg = f'Output is save at: {out_fn}'
-        click.echo(INFO.format(msg))
-        # Downloading css template file from ebook template repo and saving it
-        template = requests.get('https://raw.githubusercontent.com/OpenPecha/ebook-template/master/tsadra_template.css')
-        click.echo(template.content, file=open('template.css', 'w'))
-        # Running ebook-convert command to convert html file to .epub (From calibre)
-        chapter_Xpath = "//*[@class='tibetan-chapter']" #XPath expression to detect chapter titles.
-        font_family = "Monlam Uni Ouchan2"
-        font_size = 16
-        chapter_mark ="pagebreak"
-        os.system(f'ebook-convert {out_fn} {pecha_id}.epub --extra-css=./template.css --chapter={chapter_Xpath} --chapter-mark="{chapter_mark}" --base-font-size={font_size} --embed-font-family="{font_family}"')
-        # Removing html file and template file
-        os.system(f'rm {out_fn}')
-        os.system('rm template.css')
