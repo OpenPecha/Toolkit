49e5b897f9d370a1a402a97e87f0027acc2825e6
eroux <elie.roux@telecom-bretagne.eu>
2021-04-07 10:34:18 +0200

    add small test for catalog fetching

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 import pytest
 
 from openpecha.cli import download_pecha
+from openpecha.buda.openpecha_manager import OpenpechaManager
 
 
 @pytest.mark.skip
@@ -8,6 +9,11 @@ def test_download_pecha():
     pecha_path = download_pecha("P000800", branch="review")
     print(pecha_path)
 
+@pytest.mark.skip
+def test_download_cat():
+    opmgr = OpenpechaManager()
+    print(opmgr.get_list_of_pecha())
 
 if __name__ == "__main__":
     test_download_pecha()
+    #test_download_cat()
