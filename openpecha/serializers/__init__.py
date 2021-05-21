51feea745deb86354d4dcd8d8dfee3567566bc30
tenzin <ten13zin@gmail.com>
2021-05-21 13:04:29 +0530

    chore(serializer): remove unused serializers

--- openpecha/serializers/__init__.py
+++ openpecha/serializers/__init__.py
@@ -1,6 +1,4 @@
 from .editor import EditorSerializer
 from .epub import EpubSerializer
-from .foot_note import SerializeFootNote
 from .hfml import HFMLSerializer
-from .serializemd import SerializeMd
 from .rdf import Rdf
