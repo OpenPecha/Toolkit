97d3b4bba62167afa87f3aa5b7d2520acbfc933d
kaldan007 <kaldantenzin@gmail>
2021-08-26 12:17:11 +0530

    fix(testcase): test case imporved

--- tests/serializers/test_docx_serializer.py
+++ tests/serializers/test_docx_serializer.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
-from openpecha.serializers.docx import serialize_to_docx
+from openpecha.serializers.docx import DocxSerializer
 
 def test_serialize_docx():
     opf_path = Path("./tests/data/serialize/docx/P1.opf")
+    docx_serializer = DocxSerializer(opf_path)
     output_path = Path("./tests/data/serialize/docx")
-    assert output_path == serialize_to_docx(opf_path, output_path)
-    (output_path / "P1.docx").unlink()
+    assert output_path == docx_serializer.serialize(output_path)
+    # (output_path / "P1.docx").unlink()
