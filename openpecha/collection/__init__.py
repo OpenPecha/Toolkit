33d0ba3089eab3c8b812b2324b61d9a8a24a0f60
Tenzin <ten13zin@gmail.com>
2021-09-15 12:16:10 +0530

    Update __init__.py

--- openpecha/collection/__init__.py
+++ openpecha/collection/__init__.py
@@ -3,3 +3,13 @@ class Collections:
   
   def get_all(self):
     pass
+  
+  
+class Collection:
+  
+  def bundle_items(self, items=[]):
+    pass
+  
+  def export(self, format=None):
+    pass
+  
