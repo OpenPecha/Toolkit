f3ef1ac6295d6592ab17c66f5daefc58ee59437b
kaldan007 <kaldantenzin@gmail>
2021-09-16 15:32:46 +0530

    fix(alignment): generating segment pairs and generating aligned text added

--- openpecha/alignment/__init__.py
+++ openpecha/alignment/__init__.py
@@ -1,10 +1,68 @@
+from pathlib import Path
+
+from openpecha.cli import download_pecha
+from openpecha.utils import load_yaml
+
 
 class Alignment:
-  def __init__(self, id=None):
-    self.id = id
-  
-  def generate_segment_pairs(self):
-    pass
-  
-  def export(self, format=None):
-    pass
+    def __init__(self, id, title, alignment_type, segment_sources, segment_pairs):
+        self.id = id
+        self.title = title
+        self.alignment_type = alignment_type
+        self.segment_sources = segment_sources
+        self.segment_pairs = segment_pairs
+
+    def get_segment_src_paths(self):
+        segment_src_pecha_paths = {}
+        segment_src_pechas = self.segment_sources.keys()
+        for segment_src_pecha in segment_src_pechas:
+            segment_src_pecha_paths[segment_src_pecha] = download_pecha(
+                segment_src_pecha
+            )
+        return segment_src_pecha_paths
+
+    def get_segment_layer(self, pecha_id, pecha_path):
+        try:
+            segment_layer = load_yaml(
+                (Path(pecha_path) / f"{pecha_id}/layers/Segment.yml")
+            )
+            segment_annotations = list(segment_layer["annotations"])
+            return segment_annotations
+        except Exception:
+            return []
+
+    def get_segment_annotations(self, segment_src_paths):
+        segment_annotations = {}
+        for segment_src_pecha_id, segment_src_pecha_path in segment_src_paths.items():
+            segment_annotations[segment_src_pecha_id] = self.get_segment_layer(
+                segment_src_pecha_id, segment_src_pecha_path
+            )
+        return segment_annotations
+
+    def longest_segment(self, segment_annotations):
+        src = max(segment_annotations, key=lambda k: len(segment_annotations[k]))
+        return segment_annotations[src]
+
+    def get_cur_segment_pairs(self, segment_annotations, segment_walker):
+        cur_segment_pairs = {}
+        for src, segment_ann in segment_annotations.items():
+            try:
+                cur_segment_pairs[src] = segment_ann[segment_walker]
+            except Exception:
+                cur_segment_pairs[src] = ""
+        return cur_segment_pairs
+
+    def generate_segment_pairs(self, segment_src_paths):
+        segment_pairs = {}
+        segment_annotations = self.get_segment_annotations(segment_src_paths)
+        longest_segment_ann = self.longest_segment(segment_annotations)
+        for segment_walker, segment_id in enumerate(longest_segment_ann):
+            cur_segment_pairs = self.get_cur_segment_pairs(
+                segment_annotations, segment_walker
+            )
+            segment_pairs[f"{int(segment_walker):04}"] = cur_segment_pairs
+            cur_segment_pairs = {}
+        return segment_pairs
+
+    def export(self, format=None):
+        pass
--- openpecha/alignment/generate.py
+++ openpecha/alignment/generate.py
@@ -1,1 +1,61 @@
+from pathlib import Path
 
+from openpecha import alignment
+from openpecha.alignment import Alignment
+from openpecha.utils import load_yaml
+
+
+def get_src_bases(src_paths):
+    src_bases = {}
+    for src_name, src_path in src_paths.items():
+        src_bases[src_name] = (Path(src_path) / f"{src_name}/bases/0001.txt").read_text(
+            encoding="utf-8"
+        )
+    return src_bases
+
+
+def get_segment_layer(self, pecha_id, pecha_path):
+    try:
+        segment_layer = load_yaml((Path(pecha_path) / f"{pecha_id}/layers/Segment.yml"))
+        segment_annotations = segment_layer["annotations"]
+        return segment_annotations
+    except Exception:
+        return {}
+
+
+def get_segment_annotations(self, segment_src_paths):
+    segment_annotations = {}
+    for segment_src_pecha_id, segment_src_pecha_path in segment_src_paths.items():
+        segment_annotations[segment_src_pecha_id] = self.get_segment_layer(
+            segment_src_pecha_id, segment_src_pecha_path
+        )
+    return segment_annotations
+
+
+def get_segment(segment_ann, src_bases, segment_src):
+    segment_text = ""
+    if segment_ann:
+        src_base = src_bases[segment_src]
+        segment_start = segment_ann["span"]["start"]
+        segment_end = segment_ann["span"]["end"]
+        segment_text = src_base[segment_start:segment_end]
+    return segment_text
+
+
+def get_segment_text(segment_annotations, src_bases, segment_pair):
+    segment_text = ""
+    for segment_src, segment_id in segment_pair.items():
+        segment_ann = segment_annotations[segment_src].get(segment_id, {})
+        segment_text += get_segment(segment_ann, src_bases, segment_src) + "\n"
+    return segment_text
+
+
+def get_segment_pairs(segment_src_paths, segment_pairs):
+    alignment_text = ""
+    segment_annotations = get_segment_annotations(segment_src_paths)
+    src_bases = get_src_bases(segment_src_paths)
+    for segmetn_id, segment_pair in segment_pairs.items():
+        alignment_text += (
+            f"{get_segment_text(segment_annotations, src_bases, segment_pair)}\n\n"
+        )
+    return alignment_text
