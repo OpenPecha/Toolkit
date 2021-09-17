e93ccfc5dae148e2939695ada59eaa404de3450e
Tashi Tsering <ta3tsering@gmail.com>
2021-09-17 16:56:49 +0530

    Revert "fix(collection): get all collections, bundle and export items"
    This reverts commit d805d6f91df0333e876437db025bdd88a1d0897a.

--- tests/collections/test_export.py
+++ tests/collections/test_export.py
@@ -5,4 +5,4 @@ from openpecha import config
 def test_export():
     obj = Collection("chojuk")
     export_path = obj.export("plain")
-    assert f"{export_path}.zip" == f"{config.COLLECTIONS_PATH}/chojuk.zip"
+    assert export_path == f"{config.COLLECTIONS_PATH}/chojuk"
