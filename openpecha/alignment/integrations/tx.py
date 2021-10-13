7d861f0d27407f9bb10ad04512c0c3837f915e2f
tenzin <ten13zin@gmail.com>
2021-10-13 23:48:20 +0530

    fix(alignment-transifex): add TM to transifex project

--- openpecha/alignment/integrations/tx.py
+++ openpecha/alignment/integrations/tx.py
@@ -4,6 +4,7 @@ This module contains all the functions required for Transifex integeration
 
 import json
 import os
+import time
 from pathlib import Path
 
 import requests
@@ -50,7 +51,7 @@ class TransifexProject:
                     ],
                     "name": name,
                     "priority": "normal",
-                    "slug": slu)g
+                    "slug": slug
                 },
                 "relationships": {
                     "i18n_format": {
@@ -67,7 +68,7 @@ class TransifexProject:
                     }
                 },
                 "type": "resources"
-            })
+            }
         }
 
         response = requests.post(self.resources_api_url, headers=headers, data=json.dumps(payload))
@@ -92,9 +93,12 @@ class TransifexProject:
             return
         return resource_id
 
-    def add_resource(self, name: str, slug: str, content_path: Path):
-        if not content_path.is_file():
-            print(f"[ERROR] FileNotFound: {content_path}")
+    def add_resource(self, name: str, slug: str, source_path: Path):
+
+        print(f"[INFO] adding resource to project ({self.project.id})")
+
+        if not source_path.is_file():
+            print(f"[ERROR] FileNotFound: {source_path}")
             return
 
         resource_id = self._get_resource(name, slug)
@@ -104,12 +108,84 @@ class TransifexProject:
         resource = self.project.fetch('resources').get(slug=slug)
         transifex_api.ResourceStringsAsyncUpload.upload(
             resource=resource,
-            content=content_path.open()
+            content=source_path.open()
         )
         print(f"[INFO] Resource created at {resource_id}")
+        return resource_id
+
+    def add_translation(self, resource_id, lang:str, content_path: Path):
+        """add translation .po file to a resource"""
+
+        url = "https://rest.api.transifex.com/resource_translations_async_uploads"
+        headers = {
+            "Authorization": f"Bearer {API_TOKEN}",
+        }
+        payload = {
+            "language": f"l:{lang}",
+            "resource": resource_id
+        }
+        files = {"content": content_path.open("rb")}
+        response = requests.post(url, headers=headers, data=payload, files=files)
+
+        if response.status_code != 202:
+            print("[ERROR] response:", response.json())
+            return
+
+        # check if upload has completed after every 5 secs
+        upload_id = response.json()["data"]["id"]
+        upload_status_check_url = f"https://rest.api.transifex.com/resource_translations_async_uploads/{upload_id}"
+
+        while True:
+            response = requests.get(upload_status_check_url, headers=headers)
+            if response.status_code == 200:
+                return upload_id
+            time.sleep(5)
+
+    def remove_resource(self, slug:str):
+        """remvoes resource from the project"""
+
+        resource = self.project.fetch('resources').get(slug=slug)
+        resource_id = resource.id
+        resource.delete()
+        print(f"[INFO] Resource ({resource_id}) deleted")
+
+
+    def add_tm(self, source_path: Path, target_path: Path, target_lang="en"):
+        """add .po format translation memory to the project"""
+
+        print(f"[INFO] adding TM to project ({self.project.id})")
+
+        resource_name = "Translation Memory (don't open)"
+        resource_slug = "tm"
+
+        resource_id = self.add_resource(name=resource_name, slug=resource_slug, source_path=source_path)
+        if not resource_id:
+            print(f"[ERROR] failed to add translation memory to project {self.project.id}")
+            return
+        upload_id = self.add_translation(
+            resource_id=resource_id,
+            lang=target_lang,
+            content_path=target_path
+        )
+        if not upload_id:
+            print("[ERROR] failed to upload translation")
+            self.remove_resource(slug=resource_slug)
+
+        print(f"[INFO] Successfully added TM to project ({self.project.id})")
 
 
 
 if __name__ == "__main__":
     project = TransifexProject("esukhia", "test-2612")
-    project.add_resource("test-tm", "testtm", Path("./tests/data/alignment/test.po"))
+    # project.remove_resource(slug="tm")
+    # project.remove_resource(slug="translatethis")
+    print("[INFO] Adding source file")
+    project.add_resource(
+        name="Translate this",
+        slug="translatethis",
+        source_path=Path("./tests/data/alignment/test-bo.po"),
+    )
+    project.add_tm(
+        source_path=Path("./tests/data/alignment/test-bo.po"),
+        target_path=Path("./tests/data/alignment/test-en.po")
+    )
