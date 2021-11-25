1fba453b9312957cebc33ae896d9dff483d9fc54
tenzin <ten13zin@gmail.com>
2021-11-25 17:10:54 +0530

    fix(core): save and load workk from yaml

--- openpecha/core/work.py
+++ openpecha/core/work.py
@@ -3,11 +3,39 @@
 This module contains OpenPecha Work data model and utils for
 creating work objects.
 """
-from typing import Dict, List, Optional
+import json
+from datetime import datetime
+from pathlib import Path
+from typing import Dict, List, Union
 
 from pydantic import BaseModel, validator
+from pydantic.networks import AnyHttpUrl
 
+from ..utils import dump_yaml, load_yaml
 from .ids import get_work_id
+from .layer import InitialCreationEnum
+
+
+class InstanceMetadata(BaseModel):
+    """OpenPecha Work instance metadata."""
+
+    source_id: str
+    initial_creation_type: InitialCreationEnum
+    title: str
+    author: str = None
+    organization: AnyHttpUrl
+    created_at: datetime
+    last_modified_at: datetime
+
+
+class Instance(BaseModel):
+    """Instance model
+
+    This model represents a single instance of a work.
+    """
+
+    id: str
+    metadata: InstanceMetadata
 
 
 class Work(BaseModel):
@@ -30,3 +58,58 @@ class Work(BaseModel):
     @validator("id", pre=True, always=True)
     def set_id(cls, v):
         return v or get_work_id()
+
+    def add_instance(self, instance: Instance):
+        """Add instance to work.
+
+        Args:
+            instance (Dict): Instance to add to work.
+        """
+        self.instances[instance.id] = instance.metadata.dict()
+
+    def get_instance(self, instance_id: str) -> Instance:
+        """Get instance from work.
+
+        Args:
+            instance_id (str): Instance id to get from work.
+
+        Returns:
+            instance: Instance from work.
+        """
+        metadata = self.instances.get(instance_id)
+        if not metadata:
+            return None
+        return Instance(id=instance_id, metadata=InstanceMetadata(**metadata))
+
+    def remove_instance(self, instance_id: str):
+        """Remove instance from work.
+
+        Args:
+            instance_id (str): Instance id to remove from work.
+        """
+        del self.instances[instance_id]
+
+    @classmethod
+    def from_yaml(cls, fn: Union[str, Path]) -> "Work":
+        """Create Work from yaml file.
+
+        Args:
+            fn (str): YAML file name.
+
+        Returns:
+            Work: Work object.
+        """
+        if isinstance(fn, str):
+            fn = Path(fn)
+
+        data = load_yaml(fn)
+        work = cls.parse_obj(data)
+        for instance_id, metadata in work.instances.items():
+            work.instances[instance_id] = InstanceMetadata.parse_obj(metadata)
+        return work
+
+    def save_to_yaml(self, output_dir: Union[str, Path]) -> Path:
+        output_dir = Path(output_dir)
+        work_fn = output_dir / f"{self.id}.yaml"
+        dump_yaml(data=json.loads(self.json()), output_fn=work_fn)
+        return work_fn
--- tests/core/test_work.py
+++ tests/core/test_work.py
@@ -1,4 +1,54 @@
-from openpecha.core.work import Work
+import tempfile
+from datetime import datetime
+
+import pytest
+
+from openpecha.core.layer import InitialCreationEnum
+from openpecha.core.work import Instance, InstanceMetadata, Work
+
+
+@pytest.fixture(scope="module")
+def instance():
+    metadata = InstanceMetadata(
+        source_id="source_id",
+        initial_creation_type=InitialCreationEnum.ocr,
+        title="title",
+        author="author",
+        organization="https://bdrc.io",
+        created_at=datetime.now(),
+        last_modified_at=datetime.now(),
+    )
+    instance_ = Instance(id="instance_id", metadata=metadata)
+    return instance_
+
+
+def test_instance_metadata_model():
+    metadata = InstanceMetadata(
+        source_id="source_id",
+        initial_creation_type=InitialCreationEnum.ocr,
+        title="title",
+        author="author",
+        organization="https://bdrc.io",
+        created_at=datetime.now(),
+        last_modified_at=datetime.now(),
+    )
+
+    assert metadata.source_id == "source_id"
+
+
+def test_instance_model():
+    metadata = InstanceMetadata(
+        source_id="source_id",
+        initial_creation_type=InitialCreationEnum.ocr,
+        title="title",
+        author="author",
+        organization="https://bdrc.io",
+        created_at=datetime.now(),
+        last_modified_at=datetime.now(),
+    )
+    instance = Instance(id="instance_id", metadata=metadata)
+
+    assert instance.id == "instance_id"
 
 
 def test_work_model_with_all_attrs():
@@ -13,7 +63,6 @@ def test_work_model_with_all_attrs():
         wikidata_id="wikidata_id",
         instances={"instance_id": {"title": "title"}},
     )
-    print(work)
 
     assert work.title == "title"
 
@@ -22,3 +71,40 @@ def test_work_model_without_default_attrs():
     work = Work(title="title")
 
     assert not work.bdrc_id
+
+
+def test_add_instance(instance):
+    work = Work(title="title")
+    work.add_instance(instance)
+
+    assert instance.id in work.instances
+
+
+def test_remove_instance(instance):
+    work = Work(title="title")
+    work.add_instance(instance)
+    work.remove_instance(instance.id)
+
+    assert instance.id not in work.instances
+
+
+def test_save_work_to_yml(instance):
+    work = Work(title="title")
+    work.add_instance(instance)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        work_fn = work.save_to_yaml(tmpdirname)
+        assert work_fn.is_file()
+
+
+def test_create_work_from_yaml(instance):
+    work = Work(title="title")
+    work.add_instance(instance)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        work_fn = work.save_to_yaml(tmpdirname)
+        loaded_work = Work.from_yaml(fn=work_fn)
+
+        assert loaded_work.id == work.id
+        assert loaded_work.title == work.title
+        assert loaded_work.instances == work.instances
