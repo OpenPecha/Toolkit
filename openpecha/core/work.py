366540dab431ac5de303802e617539ff20bf34db
tenzin <ten13zin@gmail.com>
2021-11-25 16:11:52 +0530

    fix(core): add Work model and test

--- openpecha/core/work.py
+++ openpecha/core/work.py
@@ -3,7 +3,30 @@
 This module contains OpenPecha Work data model and utils for
 creating work objects.
 """
+from typing import Dict, List, Optional
+
+from pydantic import BaseModel, validator
+
+from .ids import get_work_id
 
 
-class Work:
-    pass
+class Work(BaseModel):
+    """OpenPecha Work
+
+    This class contains OpenPecha Work data model.
+    """
+
+    id: str = None
+    bdrc_id: str = None
+    title: str
+    alternative_titles: List[str] = []
+    author: str = None
+    isRoot: bool = None
+    language: str = None
+    type_defination: str = None
+    wikidata_id: str = None
+    instances: Dict[str, Dict] = {}
+
+    @validator("id", pre=True, always=True)
+    def set_id(cls, v):
+        return v or get_work_id()
--- tests/core/test_work.py
+++ tests/core/test_work.py
@@ -1,17 +1,24 @@
-from openpecha.work import Work
+from openpecha.core.work import Work
 
 
-def test_work_model():
+def test_work_model_with_all_attrs():
     work = Work(
         bdrc_work_id="bdrc_work_id",
         title="title",
         alternative_titles=["alternative_titles"],
-        authors=["authors"],
+        author="authors",
         isRoot=True,
         Language="http://purl.bdrc.io/resource/LangBo",
         type_defination="http://purl.bdrc.io/ontology/core/Work",
         wikidata_id="wikidata_id",
-        isinstances={"instance_id": {"title": "title"}},
+        instances={"instance_id": {"title": "title"}},
     )
+    print(work)
 
-    assert work
+    assert work.title == "title"
+
+
+def test_work_model_without_default_attrs():
+    work = Work(title="title")
+
+    assert not work.bdrc_id
