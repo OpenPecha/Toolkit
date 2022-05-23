76088b825d05d7a34db0cc80b298f32af6999e44
tenzin <ten13zin@gmail.com>
2022-05-23 21:58:00 +0530

    add script to find orced pecha

---
+++ scripts/create_botok_view_with_action.py
@@ -0,0 +1,65 @@
+import logging
+import os
+from pathlib import Path
+
+from github import Github, GithubException
+from tqdm import tqdm
+
+from openpecha.corpus.download import (
+    get_corpus_catalog,
+    get_corpus_items_count,
+    get_request_session,
+)
+
+logging.basicConfig(
+    filename=f"{Path(__name__).stem}.log",
+    format="%(asctime)s - %(levename)s - %(message)s",
+)
+
+
+def get_modified_content(url, session):
+    r = session.get(url)
+    return r.text + "\n"
+
+
+def get_openpecha_org():
+    token = os.environ["GITHUB_TOKEN"]
+    g = Github(token)
+    return g.get_organization("OpenPecha")
+
+
+def has_branch(branch_name, repo):
+    try:
+        repo.get_branch(branch_name)
+        return True
+    except GithubException:
+        return False
+
+
+def create_botok_view(repo, session):
+    msg = "create botok view"
+    if has_branch("botok", repo):
+        return
+    old_content = repo.get_contents(".github/workflows/create_botok_view.yml")
+    new_content = get_modified_content(old_content.download_url, session)
+    repo.update_file(old_content.path, msg, new_content, old_content.sha)
+
+
+def main():
+    org = get_openpecha_org()
+    corpus_name = "literary_bo"
+    session = get_request_session()
+    corpus_catalog = get_corpus_catalog(corpus_name, session)
+    corpus_items_count = get_corpus_items_count(corpus_name, session)
+    for corpus in tqdm(corpus_catalog, total=corpus_items_count):
+        pecha_id = corpus[0]
+        pecha_repo = org.get_repo(pecha_id)
+        try:
+            create_botok_view(pecha_repo, session)
+        except Exception as e:
+            logging.error(f"[ERROR] {pecha_id} failed!")
+            logging.exception(e)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/get_ocred_pechas_corpus_catalog.py
@@ -0,0 +1,61 @@
+import multiprocessing
+
+import requests
+import yaml
+from tqdm import tqdm
+
+from openpecha.core.layer import InitialCreationEnum, PechaMetaData
+from openpecha.corpus.download import (
+    get_corpus_catalog,
+    get_corpus_items_count,
+    get_request_session,
+)
+
+
+def _fix_old_metadata_attrs_name(metadata: str) -> str:
+    metadata = metadata.replace("created_at", "imported")
+    metadata = metadata.replace("last_modified_at", "last_modified")
+    return metadata
+
+
+def get_pecha_metadata(pecha_id: str) -> PechaMetaData:
+    url = f"https://raw.githubusercontent.com/OpenPecha/{pecha_id}/master/{pecha_id}.opf/meta.yml"
+    session = get_request_session()
+    r = session.get(url)
+
+    if r.status_code != requests.codes.OK:
+        print(f"metadata not found for pecha `{pecha_id}`")
+
+    raw_metadata = _fix_old_metadata_attrs_name(r.text)
+    metadata_dict = yaml.safe_load(raw_metadata)
+    try:
+        metadata = PechaMetaData.parse_obj(metadata_dict)
+    except Exception:
+        metadata = PechaMetaData(initial_creation_type=InitialCreationEnum.input)
+    return metadata
+
+
+def is_ocred_pecha(pecha_id: str):
+    print(f"[INFO] checking {pecha_id}...")
+    metadata = get_pecha_metadata(pecha_id)
+    return metadata.initial_creation_type == InitialCreationEnum.ocr
+
+
+def main():
+    corpus_name = "literary_bo"
+    session = get_request_session()
+    corpus_catalog = get_corpus_catalog(corpus_name, session)
+    pecha_ids = [row[0] for row in corpus_catalog]
+
+    print("Finding ocred pecha...")
+    with multiprocessing.Pool() as pool:
+        check_ocred_pecha_ids = pool.map(is_ocred_pecha, pecha_ids)
+
+    print(check_ocred_pecha_ids)
+    for pecha_id, is_ocred in zip(pecha_ids, check_ocred_pecha_ids):
+        if is_ocred:
+            print(pecha_id)
+
+
+if __name__ == "__main__":
+    main()
