7ea6333312d2b860fa255c9318dc9777f345a4fd
eroux <elie.roux@telecom-bretagne.eu>
2022-08-10 13:10:37 +0200

    WIP #166

--- openpecha/serializers/rdf.py
+++ openpecha/serializers/rdf.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import rdflib
 from rdflib import BNode, Literal, URIRef
 from rdflib.namespace import OWL, RDF, RDFS, SKOS, XSD, Namespace, NamespaceManager
-from openpecha.buda.tibetan_easy_chunker import TibetanEasyChunker
+from openpecha.buda.chunker import TibetanEasyChunker, EnglishEasyChunker
 from openpecha.core.layer import Layer, LayerEnum, PechaMetadata, SpanINFO
 
 rdf = RDF
@@ -47,22 +47,6 @@ class BUDARDFSerializer:
     def get_result(self):
         return self.lod_g
 
-    def get_bl_volinfo(self):
-        if self.bl_volinfo is not None:
-            return self.bl_volinfo
-        bases = self.openpecha.meta.bases
-        for base, binfo in bases.items():
-
-        if meta is None or "source_metadata" not in meta or "volumes" not in meta["source_metadata"]:
-            self.bl_volinfo = {}
-            return {}
-        volumes = meta["source_metadata"]["volumes"]
-        res = {}
-        for vol in volumes.values():
-            if "base_file" in vol:
-                res[vol["base_file"]] = vol
-        self.bl_volinfo = res
-        return res
 
     """
     Entry point to build the RDF graph
@@ -76,7 +60,7 @@ class BUDARDFSerializer:
             scanlname = sm["id"]
             if scanlname.startswith("bdr:"):
                 scanlname = scanlname[4:]
-            elif scanuri.startswith("http://purl.bdrc.io/resource/"):
+            elif scanlname.startswith("http://purl.bdrc.io/resource/"):
                 scanlname = scanlname[29:]
             self.add_triple(
                 bdr[self.lname], bdo["instanceReproductionOf"], bdr[scanlname]
@@ -91,14 +75,14 @@ class BUDARDFSerializer:
         if meta.statistics is not None:
             if "ocr_word_median_confidence_index" in meta.statistics:
                 self.add_triple(
-                    bdr[self.lname], bdo["OPFOCRWordMedianConfidenceIndex"], Literal(meta.statistics["ocr_word_median_confidence_index"], datatyle=XSD.float)
+                    bdr[self.lname], bdo["OPFOCRWordMedianConfidenceIndex"], Literal(meta.statistics["ocr_word_median_confidence_index"], datatype=XSD.float)
                 )
             if "ocr_word_mean_confidence_index" in meta.statistics:
                 self.add_triple(
-                    bdr[self.lname], bdo["OPFOCRWordMeanConfidenceIndex"], Literal(meta.statistics["ocr_word_mean_confidence_index"], datatyle=XSD.float)
+                    bdr[self.lname], bdo["OPFOCRWordMeanConfidenceIndex"], Literal(meta.statistics["ocr_word_mean_confidence_index"], datatype=XSD.float)
                 )
         if meta.legacy_id is not None and meta.legacy_id:
-            legacylname = f"IE0OP{self.legacy_id}"
+            legacylname = f"IE0OP{meta.legacy_id}"
             self.add_triple(bdr[legacylname], rdf.type, bdo["EtextInstance"])
             self.add_triple(bda[legacylname], rdf.type, adm["AdminData"])
             self.add_triple(bda[legacylname], adm["adminAbout"], bdr[legacylname])
@@ -113,12 +97,12 @@ class BUDARDFSerializer:
             if "batch" in oii:
                 self.add_triple(bdr[legacylname], bdo["OPFOCRBatch"], Literal(oii["batch"]))
             if "ocr_info" in oii and "timestamp" in oii["ocr_info"]:
-                self.add_triple(bdr[legacylname], bdo["OPFOCRTimeStamp"], Literal(oii["ocr_info"]["timestamp"], XSD.dateTime))
+                self.add_triple(bdr[legacylname], bdo["OPFOCRTimeStamp"], Literal(oii["ocr_info"]["timestamp"], datatype=XSD.dateTime))
         self.get_base_volumes()
         self.set_adm()
 
     def get_base_volumes(self):
-        for baselname, baseinfo in self.openpecha.meta.bases:
+        for baselname, baseinfo in self.openpecha.meta.bases.items():
             volume_string = self.openpecha.get_base(baselname)
             if len(volume_string) < 2:
                 continue
@@ -158,14 +142,14 @@ class BUDARDFSerializer:
     def set_etext(self, baselname, baseinfo, volume_number):
         volume_basename = f"{self.lname}_{baselname}"
         volume_number = int(re.search(r"\d+", baselname).group())
+        subject = bdr[f"UT{volume_basename}"]
         if "source_metadata" in baseinfo and "image_group_id" in baseinfo["source_metadata"]:
-            iglname = baseinfo["source_metadata"]
+            iglname = baseinfo["source_metadata"]["image_group_id"]
             if iglname.startswith("bdr:"):
                 iglname = iglname[4:]
-            elif iglname.startswith("http://purl.bdrc.io/resource/")
+            elif iglname.startswith("http://purl.bdrc.io/resource/"):
                 iglname = iglname[29:]
             self.add_triple(subject, bdo["eTextForImageGroup"], bdr[iglname])
-        subject = bdr[f"UT{volume_basename}"]
         self.add_triple(subject, rdf.type, bdo["Etext"])
         self.add_triple(subject, bdo["eTextInInstance"], bdr[self.lname])
         self.add_triple(
@@ -179,7 +163,7 @@ class BUDARDFSerializer:
             ),
         )
         self.set_etext_pages(baselname)
-        self.set_etext_chunks(baselname)
+        self.set_etext_chunks(baselname, baseinfo)
 
     def set_etext_pages(self, baselname):
         player = self.openpecha.get_layer(baselname, LayerEnum.pagination)
@@ -213,17 +197,44 @@ class BUDARDFSerializer:
         number = int(re.search(r"\d+", page_index).group())
         return number * 2 if page_index[-1] == "b" else (number * 2) - 1
 
-    def set_etext_chunks(self, volume_name):
-        volume_string = self.openpecha.get_base(volume_name)
-        chunk_indexes = self.get_chunk_index(volume_string)
 
+    def add_chunks(self, baselname, volume_string, language, previous_i = 0, start_cc=0, end_cc=-1):
+        chunk_indexes = self.get_chunk_indexes(volume_string, language, start_cc, end_cc)
         for i in range(0, len(chunk_indexes) - 2):
             self.set_etext_chunk(
-                i, chunk_indexes[i], chunk_indexes[i + 1], volume_name, volume_string
+                previous_i + i, chunk_indexes[i], chunk_indexes[i + 1], baselname, volume_string, language
             )
+        return previous_i + len(chunk_indexes) - 2
 
-    def set_etext_chunk(self, i, start_char, end_char, volume_name, volume_string):
-        volume_basename = f"{self.lname}_{volume_name}"
+
+    def set_etext_chunks(self, baselname, baseinfo):
+        volume_string = self.openpecha.get_base(baselname)
+        
+        default_language = "bo"
+        if "default_language" in baseinfo:
+            default_language = baseinfo["default_language"]
+        elif self.openpecha.meta.default_language is not None:
+            default_language = self.openpecha.meta.default_language
+
+        llayer = self.openpecha.get_layer(baselname, LayerEnum.language)
+        if llayer is None:
+            self.add_chunks(baselname, volume_string, default_language)
+        else:
+            last_index = 0
+            ci = 0
+            # for our purpose it's crucial that the annotations are returned sorted:
+            sorted_annotations = sorted(llayer.annotations.values(), key=lambda x: x["span"]["start"])
+            for annotation in sorted_annotations:
+                if annotation["span"]["start"] > last_index:
+                    ci = self.add_chunks(baselname, volume_string, default_language, ci, last_index, annotation["span"]["start"])
+                ci = self.add_chunks(baselname, volume_string, annotation["language"], ci, annotation["span"]["start"], annotation["span"]["end"])
+                last_index = annotation["span"]["end"]
+            len_vol_str = len(volume_string)
+            if last_index < len_vol_str:
+                self.add_chunks(baselname, volume_string, default_language, ci, last_index, len_vol_str)
+
+    def set_etext_chunk(self, i, start_char, end_char, baselname, volume_string, language):
+        volume_basename = f"{self.lname}_{baselname}"
         etext = f"UT{volume_basename}"
         subject = bdr[f"UT{volume_basename}_{int(i):05}"]
 
@@ -231,7 +242,7 @@ class BUDARDFSerializer:
         self.add_triple(
             subject,
             bdo["chunkContents"],
-            Literal(volume_string[start_char:end_char], lang="bo"),
+            Literal(volume_string[start_char:end_char], lang=language),
         )
         self.add_triple(
             subject, bdo["sliceEndChar"], Literal(end_char, datatype=XSD.integer)
@@ -246,18 +257,24 @@ class BUDARDFSerializer:
         subject_r = bda[self.lname]
         einst_r = bdr[self.lname]
         graph_r = bdg[self.lname]
-        rev = self.openpecha.get_rev()
+        if hasattr(self.openpecha, 'get_rev'):
+            rev = self.openpecha.get_rev()
+            self.add_triple(subject_r, adm["gitRevision"], Literal(rev))
 
         self.add_triple(subject_r, rdf.type, adm["AdminData"])
         self.add_triple(subject_r, adm["adminAbout"], einst_r)
         self.add_triple(subject_r, adm["graphId"], graph_r)
         self.add_triple(subject_r, adm["syncAgent"], bdr["SAOPT"])
         self.add_triple(subject_r, adm["metadataLegal"], bda["LD_BDRC_CC0"])
-        self.add_triple(subject_r, adm["gitRevision"], Literal(rev))
+        
 
     @staticmethod
-    def get_chunk_index(string):
-        chunker = TibetanEasyChunker(string, 1500)
+    def get_chunk_indexes(string, language, start_char = 0, end_char = 0):
+        chunker = None
+        if language == "bo":
+            chunker = TibetanEasyChunker(string, 1500, start_char, end_char)
+        else:
+            chunker = EnglishEasyChunker(string, 1500, start_char, end_char)
         indexes = chunker.get_chunks()
 
         return indexes
