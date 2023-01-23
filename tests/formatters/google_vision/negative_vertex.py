90b5dd6cbf37aef0f621a569eea8fd66d96b29c9
Elie Roux <roux.elie@gmail.com>
2023-01-23 07:56:12 +0100

    fix(google_vision): Fix rare case in Google Vision json output (#236)
    * added test to recreate a google vision bug
    
    * rename test, fix code
    
    * fix(google_vision): handle negative vertex coordinates
    
    Co-authored-by: Tashi Tsering <tashitsering@Tashis-MacBook-Pro.local>

---
+++ tests/formatters/google_vision/negative_vertex.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+from openpecha.formatters.ocr.ocr import OCRFormatter
+from openpecha.formatters.ocr.google_vision import GoogleVisionFormatter, GoogleVisionBDRCFileProvider
+from openpecha.utils import load_yaml
+
+
+ocr_path = Path(__file__).parent / "data" / "36940497.json"
+
+def negative_x_vertex():
+
+    # The json file has a vertex with an x coordinate of -1
+
+    state = {
+        "base_layer_len": 0,
+        "base_layer": "",
+        "low_confidence_annotations": {},
+        "language_annotations": [],
+        "pagination_annotations": {},
+        "word_confidences": [],
+        "latest_language_annotation": None,
+        "latest_low_confidence_annotation": None,
+        "page_low_confidence_annotations": []
+    }
+
+    ocr_object = load_yaml(ocr_path)
+    
+    google_formatter = GoogleVisionFormatter()
+    
+    bboxes, avg_char_width = google_formatter.get_char_base_bboxes_and_avg_width(response=ocr_object)
+    
+    ocr_formatter = OCRFormatter()
+    ocr_formatter.remove_duplicate_symbols = True
+    ocr_formatter.same_line_ratio_threshold = 0.2
+
+    ocr_formatter.build_page(bboxes, 1, "36940497", state, avg_char_width)
+    base = state['base_layer']
+
+
+if __name__ == "__main__":
+    negative_x_vertex()
